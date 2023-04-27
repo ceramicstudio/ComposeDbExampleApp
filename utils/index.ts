ba4f187084dd2e1807ab22aa2f7e9b7b8204aa5f
gzalz <ericdaniel.gonzalez@gmail.com>
2023-04-27 16:49:44 -0600

    working inmemory using hardcoded keyDID

--- utils/index.ts
+++ utils/index.ts
@@ -1,53 +1,24 @@
-import { DIDSession } from "did-session";
-import { EthereumWebAuth, getAccountId } from "@didtools/pkh-ethereum";
 import type { CeramicApi } from "@ceramicnetwork/common"
 import type { ComposeClient } from "@composedb/client";
-
-// If you are relying on an injected provider this must be here otherwise you will have a type error. 
-declare global {
-  interface Window {
-    ethereum: any;
-  }
-}
+import {Ed25519Provider} from "key-did-provider-ed25519";
+import { getResolver } from 'key-did-resolver'
+import {DID} from "dids";
 
 /**
  * Checks localStorage for a stored DID Session. If one is found we authenticate it, otherwise we create a new one.
  * @returns Promise<DID-Session> - The User's authenticated sesion.
  */
 export const authenticateCeramic = async (ceramic: CeramicApi, compose: ComposeClient) => {
-  const sessionStr = localStorage.getItem('did') // for production you will want a better place than localStorage for your sessions.
-  let session
-
-  if(sessionStr) {
-    session = await DIDSession.fromSession(sessionStr)
-  }
-
-  if(!session || (session.hasSession && session.isExpired)) {
-    if (window.ethereum === null || window.ethereum === undefined) {
-      throw new Error("No injected Ethereum provider found.");
-    }
-
-    // We enable the ethereum provider to get the user's addresses.
-    const ethProvider = window.ethereum;
-    // request ethereum accounts.
-    const addresses = await ethProvider.enable({
-      method: "eth_requestAccounts",
-    });
-    const accountId = await getAccountId(ethProvider, addresses[0])
-    const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId)
-
-    /**
-     * Create DIDSession & provide capabilities that we want to access.
-     * @NOTE: Any production applications will want to provide a more complete list of capabilities.
-     *        This is not done here to allow you to add more datamodels to your application.
-     */
-    session = await DIDSession.authorize(authMethod, {resources: ["ceramic://*"]})
-    // Set the session in localStorage.
-    localStorage.setItem('did', session.serialize());
-  }
-
-  // Set our Ceramic DID to be our session DID.
-  compose.setDID(session.did)
-  ceramic.did = session.did
-  return
+  const seed = new Uint8Array([
+    57, 105,  44, 154, 126, 106, 138,  36,
+    6,  29,  62, 191,  79,  25,   5, 224,
+    235,   5, 203, 135, 153,   8, 178, 232,
+    150, 204, 227, 242, 251,  57, 103,  35
+  ])
+  const provider = new Ed25519Provider(seed)
+  const did = new DID({ provider, resolver: getResolver() })
+  await did.authenticate()
+  ceramic.did = did
+  compose.setDID(did)
+  return did
 }
