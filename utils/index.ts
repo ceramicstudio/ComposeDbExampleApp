2152ca7eed8597a2cc0212f741b5030241189507
gzalz <ericdaniel.gonzalez@gmail.com>
2023-04-28 09:36:53 -0600

    key did auth, needs form validation

--- utils/index.ts
+++ utils/index.ts
@@ -4,18 +4,33 @@ import {Ed25519Provider} from "key-did-provider-ed25519";
 import { getResolver } from 'key-did-resolver'
 import {DID} from "dids";
 
+const DID_SEED_KEY = 'ceramic_did_seed'
+
 /**
  * Checks localStorage for a stored DID Session. If one is found we authenticate it, otherwise we create a new one.
  * @returns Promise<DID-Session> - The User's authenticated sesion.
  */
 export const authenticateCeramic = async (ceramic: CeramicApi, compose: ComposeClient) => {
-  const seed = new Uint8Array([
-    57, 105,  44, 154, 126, 106, 138,  36,
-    6,  29,  62, 191,  79,  25,   5, 224,
-    235,   5, 203, 135, 153,   8, 178, 232,
-    150, 204, 227, 242, 251,  57, 103,  35
-  ])
-  const provider = new Ed25519Provider(seed)
+  let seed_array: Uint8Array
+  if (localStorage.getItem(DID_SEED_KEY) === null){
+    console.log("Generating seed...")
+    let seed = crypto.getRandomValues(new Uint8Array(32))
+    let seed_json = JSON.stringify(seed, (key, value) => {
+      if (value instanceof Uint8Array) {
+        return Array.from(value);
+      }
+      return value;
+    });
+    localStorage.setItem(DID_SEED_KEY, seed_json)
+    seed_array = seed
+    console.log("Generated new seed: " + seed)
+  } else {
+    let seed_json_value = localStorage.getItem(DID_SEED_KEY)
+    let seed_object = JSON.parse(seed_json_value)
+    seed_array = seed_object
+    console.log("Found seed: " + seed_array)
+  }
+  const provider = new Ed25519Provider(seed_array)
   const did = new DID({ provider, resolver: getResolver() })
   await did.authenticate()
   ceramic.did = did
