29509fadfdb58314d603b7cb2b40f711bc0995ab
Mark Krasner <markkrasner@Marks-MacBook.local>
2023-09-22 15:34:25 -0400

    Edits to event listener

--- scripts/run.mjs
+++ scripts/run.mjs
@@ -12,7 +12,7 @@ await spawn("npm", ["run", "generate"]);
 const ceramic = spawn("npm", ["run", "ceramic"]);
 ceramic.stdout.on("data", (buffer) => {
   console.log('[Ceramic]', buffer.toString())
-  if (buffer.toString().includes("0.0.0.0:7007")) {
+  if (buffer.toString().includes("IMPORTANT: Ceramic API running")) {
     events.emit("ceramic", true);
     spinner.succeed("ceramic node started");
   }
